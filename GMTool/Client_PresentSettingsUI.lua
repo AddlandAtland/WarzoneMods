a90459bc7ee9e593df7cfd1584859a50fb5daf3b
AddlandAtland <dameandadine@gmail.com>
2024-12-03 11:06:52 +1300

    Update Client_PresentSettingsUI.lua

--- GMTool/Client_PresentSettingsUI.lua
+++ GMTool/Client_PresentSettingsUI.lua
@@ -1,14 +1,4 @@
-function Client_PresentConfigureUI(rootParent)
-	local initialValue = Mod.Settings.CostPerNeutralArmy;
-	if initialValue == nil then
-		initialValue = 0;
-	end
-    
-    local horz = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz).SetText('Cost per neutral army on the territory');
-    numberInputField = UI.CreateNumberInputField(horz)
-		.SetSliderMinValue(0)
-		.SetSliderMaxValue(8)
-		.SetValue(initialValue);
-
+function Client_PresentSettingsUI(rootParent)
+	UI.CreateLabel(rootParent)
+		.SetText('Cost per neutral army = ' .. Mod.Settings.CostPerNeutralArmy);
 end
