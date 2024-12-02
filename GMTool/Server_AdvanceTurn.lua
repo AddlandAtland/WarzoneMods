3a914175f7c073b91127f42c1862e75dbd8a7c34
AddlandAtland <dameandadine@gmail.com>
2024-12-03 11:55:12 +1300

    Update Server_AdvanceTurn.lua

--- GMTool/Server_AdvanceTurn.lua
+++ GMTool/Server_AdvanceTurn.lua
@@ -4,7 +4,7 @@ function Server_AdvanceTurn_Order(game, order, result, skipThisOrder, addNewOrde
 
     local gmos = Mod.Settings.GMosID
 
-    if (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'GMTool_')) then  --look for the order that we inserted in Client_PresentMenuUI
+    if (order.proxyType == 'GameOrderCustom' and startsWith(order.Payload, 'GiftArmies2_')) then  --look for the order that we inserted in Client_PresentMenuUI
 
 		--in Client_PresentMenuUI, we comma-delimited the number of armies, the target territory ID, and the target player ID.  Break it out here
 		local payloadSplit = split(string.sub(order.Payload, 13), ','); 
