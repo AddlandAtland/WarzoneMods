cec7ca6f0b2a6b6c0041cb017887f4c6c37ccd40
AddlandAtland <dameandadine@gmail.com>
2024-12-03 11:45:29 +1300

    Update Client_PresentConfigureUI.lua

--- GMTool/Client_PresentConfigureUI.lua
+++ GMTool/Client_PresentConfigureUI.lua
@@ -1,14 +1,14 @@
 function Client_PresentConfigureUI(rootParent)
-	local num = Mod.Settings.GMosID;
-	if num == nil then
-		num = 0;
+	local initialValue = Mod.Settings.CostPerNeutralArmy;
+	if initialValue == nil then
+		initialValue = 0;
 	end
     
     local horz = UI.CreateHorizontalLayoutGroup(rootParent);
-	UI.CreateLabel(horz).SetText('Set the Host ID');
+	UI.CreateLabel(horz).SetText('Cost per neutral army on the territory');
     numberInputField = UI.CreateNumberInputField(horz)
 		.SetSliderMinValue(0)
-		.SetSliderMaxValue(100)
-		.SetValue(num);
+		.SetSliderMaxValue(8)
+		.SetValue(initialValue);
 
 end
