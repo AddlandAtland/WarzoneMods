156d42f87879d5f59beb3392175bfcef11724198
AddlandAtland <dameandadine@gmail.com>
2024-12-06 14:36:28 +1300

    Update Client_PresentMenuUI.lua

--- GMTool/Client_PresentMenuUI.lua
+++ GMTool/Client_PresentMenuUI.lua
@@ -116,7 +116,7 @@ function SubmitClicked()
 		msg = 'Neutralizing ' .. SelectedTerritory.Name;
 	end
 
-	local payload = 'GMTool_' .. NumArmiesInput.GetValue() .. ',' .. SelectedTerritory.ID .. ',' .. TargetPlayerID .. ',' .. CitiesInput;
+	local payload = 'GMTool_' .. NumArmiesInput.GetValue() .. ',' .. SelectedTerritory.ID .. ',' .. TargetPlayerID .. ',' .. CitiesInput.GetValue();
 
 	local orders = Game.Orders;
 	table.insert(orders, WL.GameOrderCustom.Create(Game.Us.ID, msg, payload));
